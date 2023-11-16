55a7bbf07dca5d2a30a1810188c9c56f330cd3c7
lRebornsl <anthony_vasquez98@hotmail.com>
2023-11-16 17:16:40 -0500

    Modify Greeting component

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreeting } from '../redux/Greeting/greetingSlice';
+
 const Greeting = () => {
+  const dispatch = useDispatch();
+  const greeting = useSelector((state) => state.greetingSlice.greeting.greeting);
+
+  useEffect(() => {
+      dispatch(getGreeting());
+  }, [dispatch]);
+
   return (
       <div>
-          <h1>Hello, World!</h1>
+          <h1>{greeting}</h1>
       </div>
   );
 }
